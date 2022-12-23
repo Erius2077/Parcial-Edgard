d9acbb7e8f3f39a4921c52f4548bb19c319c22ff
Edgard Perez <edgardperez2077@gmail.com>
2022-12-23 04:27:52 -0600

    Xd

---
+++ Pasteleria/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Pasteleria.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Pasteleria.Data
+{
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Pasteleria.Models.UserRole> UserRole { get; set; } = default!;
+        public DbSet<Pasteleria.Models.Client> Client { get; set; } = default!;
+        public DbSet<Pasteleria.Models.Cake> Treatment { get; set; } = default!;
+        public DbSet<Pasteleria.Models.CalendaryTime> TreatmentTime { get; set; } = default!;
+        public DbSet<Pasteleria.Models.Calendary> Schedule { get; set; } = default!;
+        public DbSet<Pasteleria.Models.ReservationStatus> BReservationStatus { get; set; } = default!;
+        public DbSet<Pasteleria.Models.Reservation> Booking { get; set; } = default!;
+    }
+    //public DbSet<Pasteleria.Models.UserRole> UserRole { get; set; } = default!;
+
+}
+}
